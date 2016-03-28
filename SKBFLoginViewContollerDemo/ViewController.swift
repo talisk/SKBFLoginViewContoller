cef8d4f717a8fcacab0b9c87f410391507a57433
talisk <talisk@foxmail.com>
2016-03-28 22:04:27 +0800

    Initial Commit

---
+++ SKBFLoginViewContollerDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SKBFLoginViewContollerDemo
+//
+//  Created by 孙恺 on 16/3/28.
+//  Copyright © 2016年 sunkai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
